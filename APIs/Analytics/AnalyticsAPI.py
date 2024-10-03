45b9f4029dd67c37149a36768a3bb4a26e348b5b
jonas <jonaskuzia@gmail.com>
2024-10-03 21:34:05 +0200

    optimized code

--- APIs/Analytics/AnalyticsAPI.py
+++ APIs/Analytics/AnalyticsAPI.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter, HTTPException
-
 from Clickhouse.clickhouse_client import clickhouse_client
-
 router = APIRouter()
 
 
+# Abfrage 1
 @router.get("/analytics/MedicationPatient", status_code=200)
 async def medication_patient(medication_id: int):
     try:
@@ -28,6 +27,51 @@ async def medication_patient(medication_id: int):
         raise HTTPException(status_code=500, detail=f"Error processing analytics request: {str(e)}")
 
 
+# Abfrage 2
+@router.get("/analytics/PatientOrganizationContact", status_code=200)
+async def patient_organization_contact(patient_id: int, organization_id: int):
+    try:
+        client = clickhouse_client()
+
+        query = """
+            SELECT patient.id, encounter_id
+            FROM FHIROptimization.Encounter
+            WHERE arrayExists(patient -> patient = toString(%(patient_id)s), patient.id)
+            AND arrayExists(service_provider -> service_provider = toString(%(organization_id)s), service_provider.id)
+        """
+
+        result = client.query(query, parameters={"patient_id": patient_id, "organization_id": organization_id})
+
+        patient_ids = [row[0] for row in result.result_rows]
+        encounter_ids = [row[1] for row in result.result_rows]
+
+        return {"patient_ids": patient_ids, "encounter_ids": encounter_ids}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error processing analytics request: {str(e)}")
+
+
+# Abfrage 3
+@router.get("/analytics/EncounterTimespan", status_code=200)
+async def encounter_timespan(start: str, end: str):
+    try:
+        client = clickhouse_client()
+
+        query = """
+            SELECT encounter_id
+            FROM FHIROptimization.Encounter
+            WHERE period_start > toString(%(start)s) AND period_end < toString(%(end)s)
+        """
+
+        result = client.query(query, parameters={"start": start, "end": end})
+
+        encounter_ids = [row[0] for row in result.result_rows]
+
+        return {"encounter_ids": encounter_ids, "start": start, "end": end}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error processing analytics request: {str(e)}")
+
+
+# Abfrage 4
 @router.get("/analytics/MedicationManufacturer", status_code=200)
 async def medication_manufacturer(medication_id: int, manufacturer_id: int):
     try:
@@ -50,25 +94,4 @@ async def medication_manufacturer(medication_id: int, manufacturer_id: int):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error processing analytics request: {str(e)}")
 
-
-@router.get("/analytics/PatientOrganizationContact", status_code=200)
-async def medication_manufacturer(patient_id: int, organization_id: int):
-    try:
-        client = clickhouse_client()
-
-        query = """
-            SELECT patient.id, encounter_id
-            FROM FHIROptimization.Encounter
-            WHERE arrayExists(patient -> patient = toString(%(patient_id)s), patient.id)
-            AND arrayExists(service_provider -> service_provider = toString(%(organization_id)s), service_provider.id)
-        """
-
-        result = client.query(query, parameters={"patient_id": patient_id, "organization_id": organization_id})
-
-        patient_ids = [row[0] for row in result.result_rows]
-        encounter_ids = [row[1] for row in result.result_rows]
-
-        return {"patient_ids": patient_ids, "encounter_ids": encounter_ids}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error processing analytics request: {str(e)}")
 
